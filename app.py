61389740de6ca041800500d14b10a84feab2afb6
alf808 <alf@akademe.net>
2019-08-10 23:55:03 -0400

    updated prompt layout

--- app.py
+++ app.py
@@ -11,9 +11,12 @@ import argparse
 from pyattck import Attck
 import sys
 import random
-attack = Attck()
 
+# global variables
+attack = Attck()
 error_list = ["You Chose Poorly", "Jen says: Are you fricking serious!", "Smooth move exlax!", "Hey lolo make mo bettah choice!"]
+
+
 def grab_techniques(att):
     '''this will grab techniques used by threat actors'''
     for technique in attack.techniques:
@@ -49,17 +52,20 @@ def grab_malware(mal):
 
 def print_actors():
     actor_list = [act.name for act in attack.actors]
-    print(actor_list)
+    print("Here is a sample list of actors.")
+    print(*actor_list, sep = "\t")
 
 
 def print_malwares():
+    print("Here is a sample list of malware.")
     malware_list = [mal.name for mal in attack.malwares]
-    print(malware_list)
+    print(*malware_list, sep = "\t")
 
 
 def print_attacks():
+    print("Here is a sample list of attack techniques.")
     attack_list = [att.name for att in attack.techniques]
-    print(attack_list)
+    print(*attack_list, sep = "\t")
     
 
 def main():
