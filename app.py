4b39f4c2220f148582aed69307f0488da640db9a
usnchief <james.d.jensen@gmail.com>
2019-08-11 01:07:31 +0000

    Added Menu input

--- app.py
+++ app.py
@@ -12,13 +12,15 @@ Sample:
 '''
 import argparse
 from pyattck import Attck
+import sys
 attack = Attck()
 
 def grab_techniques(att):
     '''this will grab techniques used by threat actors'''
     for technique in attack.techniques:
         if technique.name == att:
-            print(f"Attack technique name: {technique.name}")
+            print(f"Attack technique name:   {technique.name}")
+            print("Description: " + technique.description)
             print("These are mitigations: ")
             for mitigation in technique.mitigation:
                 print('\t' + mitigation.name)
@@ -29,6 +31,7 @@ def grab_actor(act):
     for actor in attack.actors:
         if actor.name == act:
             print('Actor name: ' + actor.name)
+            print('Description: ' + actor.description)
             print('Techniques used:')
             for technique in actor.techniques:
                 print('\t' + technique.name)
@@ -38,25 +41,42 @@ def grab_malware(mal):
     '''grab malware and attack techniques'''
     for mw in attack.malwares:
         if mw.name == mal:
-            print(f'Malware: ' + mw.name)
+            print('Malware: ' + mw.name)
+            print('Description: ' + mw.description)
             print('Attack techniques associated with: ' + mw.name)
             for technique in mw.techniques:
                 print('\t' + technique.name)
+def main():
+    choice = "0"
+    while True:
+        print("Main Choice: Choose 1 of 3 choices")
+        print("Choose 1 for Threat Actor")
+        print("Choose 2 for Type of Malware")
+        print("Choose 3 for Technique Mitigation")
+        print("Choose 4 to exit")
 
+        choice = input("Enter your choice:  ")
+
+        if choice == "1":
+            type = input("Input Actor: ")
+            grab_actor(type)
+        elif choice == "2":
+            type = input("Input Malware: ")
+            grab_malware(type)
+        elif choice == "3":
+            type = input("Attack to mitigate: ")
+            grab_techniques(type)
+        elif choice == "4":
+            sys.exit("Thanks for wasting our time")
+        else:
+            print("Read this is Jens voice: Are you fricking serious!")
+            
+            
 
 if __name__ == "__main__":
+    main()
     
-    parser = argparse.ArgumentParser(description="")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('-actor', help="actor will return techniques")
-    group.add_argument('-mal', help="malware will return attack technique")
-    group.add_argument('-attack', help="attack will return mitigation techniques")
 
-    args = parser.parse_args()
+    
 
-    if args.actor:
-        grab_actor(args.actor)
-    elif args.attack:
-        grab_techniques(args.attack)
-    else:
-        grab_malware(args.mal)
+    
