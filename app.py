b9fae7734e752f5c882f296aba96027aa54e428a
alf808 <alf@akademe.net>
2019-08-10 22:08:08 -0400

    added to list all choices

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ attack = Attck()
 def grab_techniques(att):
     '''this will grab techniques used by threat actors'''
     for technique in attack.techniques:
-        if technique.name == att:
-            print(f"Attack technique name:   {technique.name}")
+        if technique.name.lower() == att.lower():
+            print("Attack technique name: " + technique.name)
             print("Description: " + technique.description)
             print("These are mitigations: ")
             for mitigation in technique.mitigation:
@@ -29,7 +29,7 @@ def grab_techniques(att):
 def grab_actor(act):
     '''find if actor exists in database then invoke techniques'''
     for actor in attack.actors:
-        if actor.name == act:
+        if actor.name.lower() == act.lower():
             print('Actor name: ' + actor.name)
             print('Description: ' + actor.description)
             print('Techniques used:')
@@ -40,37 +40,53 @@ def grab_actor(act):
 def grab_malware(mal):
     '''grab malware and attack techniques'''
     for mw in attack.malwares:
-        if mw.name == mal:
+        if mw.name.lower() == mal.lower():
             print('Malware: ' + mw.name)
             print('Description: ' + mw.description)
             print('Attack techniques associated with: ' + mw.name)
             for technique in mw.techniques:
                 print('\t' + technique.name)
+
+
+def print_actors():
+    actor_list = [act.name for act in attack.actors]
+    print(actor_list)
+
+
+def print_malwares():
+    malware_list = [mal.name for mal in attack.malwares]
+    print(malware_list)
+
+
+def print_attacks():
+    attack_list = [att.name for att in attack.techniques]
+    print(attack_list)
+    
+
 def main():
     choice = "0"
     while True:
-        print("Main Choice: Choose 1 of 3 choices")
-        print("(1) for Threat Actor\t (2) Type of Malware\t (3) Technique Mitigation\t (4) Exit")
-        #print("Choose 2 for Type of Malware")
-       # print("Choose 3 for Technique Mitigation")
-       # print("Choose 4 to exit")
+        print("\nMenu Choices:")
+        print("(1) Threat Actor\t (2) Type of Malware\t (3) Attack to Mitigate\t (4) Exit")
 
         choice = input("Enter your choice:  ")
 
         if choice == "1":
+            print_actors()
             type = input("Input Actor: ")
             grab_actor(type)
         elif choice == "2":
+            print_malwares()
             type = input("Input Malware: ")
             grab_malware(type)
         elif choice == "3":
+            print_attacks()
             type = input("Attack to mitigate: ")
             grab_techniques(type)
         elif choice == "4":
             sys.exit("Thanks for wasting our time")
         else:
             print("Read this is Jens voice: Are you fricking serious!")
-            
             
 
 if __name__ == "__main__":
